2e64560b235a0ec69bca1610de80cb048cff993f
Bo <omuqqz@gmail.com>
2023-09-16 11:42:55 +0300

    feat: move colors to separate file

---
+++ src/constants/colors.ts
@@ -0,0 +1,11 @@
+export const colors = {
+  lightBlue: '#2F80ED1A',
+  blue: '#2F80ED99',
+  hardBlue: '#2F80ED',
+  lightGray: '#F1F1F1',
+  gray: '#AAAAAA',
+  orange: '#f93',
+  red: '#FF0000',
+  black: '#000',
+  white: '#fff',
+};
--- src/constants/defaultStyles.ts
+++ src/constants/defaultStyles.ts
@@ -1,67 +1,73 @@
 import { CalendarStyles } from '@/types/calendar';
 
-const lightBlue = '#2F80ED1A';
-const blue = '#2F80ED99';
-const hardBlue = '#2F80ED';
-const lightGray = '#F1F1F1';
-const gray = '#AAAAAA';
-const orange = '#f93';
+import { colors } from './colors';
+
+const {
+  lightBlue,
+  blue,
+  hardBlue,
+  lightGray,
+  gray,
+  orange,
+  red,
+  white,
+  black,
+} = colors;
+
 const selectedBorderRadius = '8px';
 const defaultBorderRadius = '0';
 
 const getLeftBorderRadius = (radius: string) => ({
-    borderBottomLeftRadius: radius,
-    borderTopLeftRadius: radius,
+  borderBottomLeftRadius: radius,
+  borderTopLeftRadius: radius,
 });
 
 const getRightBorderRadius = (radius: string) => ({
-    borderBottomRightRadius: radius,
-    borderTopRightRadius: radius,
+  borderBottomRightRadius: radius,
+  borderTopRightRadius: radius,
 });
 
 export const defaultStyles: Required<CalendarStyles> = {
-    innerDay: {
-        color: 'black',
-    },
-    outerDay: {
-        color: gray,
-    },
-    selectedDay: {
-        color: hardBlue,
-        backgroundColor: lightBlue,
-        ...getLeftBorderRadius(defaultBorderRadius),
-        ...getRightBorderRadius(defaultBorderRadius),
-    },
-    selectionHeadDay: {
-        color: 'white',
-        border: 'none',
-        backgroundColor: blue,
-        ...getLeftBorderRadius(selectedBorderRadius),
-    },
-    selectionTailDay: {
-        color: 'white',
-        border: 'none',
-        backgroundColor: hardBlue,
-        ...getRightBorderRadius(selectedBorderRadius),
-    },
-    today: {
-        backgroundColor: lightGray,
-        ...getRightBorderRadius(selectedBorderRadius),
-        ...getLeftBorderRadius(selectedBorderRadius),
-    },
-    weekend: {
-        color: 'red',
-    },
-    holiday: {
-        color: orange,
-        borderRight: '3px solid ' + orange,
-        borderBottom: '3px solid ' + orange,
-    },
-    withTodoDay: {
-        borderBottom: '1px solid black',
-    },
-    calendar: {
-        color: 'black',
-        backgroundColor: 'white',
-    }
+  innerDay: {
+    color: black,
+  },
+  outerDay: {
+    color: gray,
+  },
+  selectedDay: {
+    color: hardBlue,
+    backgroundColor: lightBlue,
+    ...getLeftBorderRadius(defaultBorderRadius),
+    ...getRightBorderRadius(defaultBorderRadius),
+  },
+  selectionHeadDay: {
+    color: white,
+    border: 'none',
+    backgroundColor: blue,
+    ...getLeftBorderRadius(selectedBorderRadius),
+  },
+  selectionTailDay: {
+    color: white,
+    border: 'none',
+    backgroundColor: hardBlue,
+    ...getRightBorderRadius(selectedBorderRadius),
+  },
+  today: {
+    backgroundColor: lightGray,
+    ...getRightBorderRadius(selectedBorderRadius),
+    ...getLeftBorderRadius(selectedBorderRadius),
+  },
+  weekend: {
+    color: red,
+  },
+  holiday: {
+    color: orange,
+  },
+  withTodoDay: {
+    borderBottom: '1px solid black',
+  },
+  calendar: {
+    color: black,
+    backgroundColor: white,
+  },
 };
