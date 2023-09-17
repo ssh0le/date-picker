61cf8dc7a64a737110f76697a2bd858c014464c6
Bo <omuqqz@gmail.com>
2023-09-17 17:59:49 +0300

    fix: apply prettier to constants

--- src/constants/holidaysApi.ts
+++ src/constants/holidaysApi.ts
@@ -1,7 +1,9 @@
 const apiKey = process.env.HOLIDAYS_KEY as string;
 export const params = {
-    year: "2023",
-    country: "by",
-    api_key: apiKey,
-}
-export const apiUrl = `https://calendarific.com/api/v2/holidays?${new URLSearchParams(params)}`;
+  year: '2023',
+  country: 'by',
+  api_key: apiKey,
+};
+export const apiUrl = `https://calendarific.com/api/v2/holidays?${new URLSearchParams(
+  params,
+)}`;
