3c085f3f5b03f8d16e3f63441823b2e4267157bf
Bo <omuqqz@gmail.com>
2023-10-03 15:01:43 +0300

    fix: remove unused icons file

--- src/constants/inputIcons.ts
+++
@@ -1,9 +0,0 @@
-import calendar from '@/assets/icons/Calendar.svg';
-import clear from '@/assets/icons/Clear.svg';
-import ok from '@/assets/icons/ok.svg';
-
-export const inputIcons = {
-  calendar,
-  clear,
-  ok,
-};
